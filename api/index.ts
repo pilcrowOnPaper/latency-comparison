6d84f04e9b957c9ef3f1466a6302d4028b49cf5e
lucythecat <pilcrowonpaper@gmail.com>
2023-11-15 12:48:25 +0900

    fix js again

--- api/index.ts
+++ api/index.ts
@@ -21,7 +21,7 @@ const html = `
         <p id="eu-west-1">eu-west-1:</p>
     </body>
     <script>
-        Promise.all([usEast1(), apNorthEast1(),apSouthEast1(), euWest1(]));
+        Promise.all([usEast1(), apNorthEast1(),apSouthEast1(), euWest1()]);
 
         async function usEast1() {
             document.getElementById("us-east-1").innerText = "us-east-1:"
